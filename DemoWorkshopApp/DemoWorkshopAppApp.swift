4390adde48926c6e086924e0ea0b405d63c64c4d
Steven Wijaya <steven.wijaya2001@gmail.com>
2022-08-19 16:49:54 +0700

    Initial Commit

---
+++ DemoWorkshopApp/DemoWorkshopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoWorkshopAppApp.swift
+//  DemoWorkshopApp
+//
+//  Created by Steven Wijaya on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoWorkshopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
